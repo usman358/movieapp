f3ff9413cb826fb2603a15863e23feec34900e74
usman358 <usman_khalid35@yahoo.com>
2022-08-21 22:43:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./Sidebar";
+import Table from "./Table";
+import image1 from "./qq.jpg";
+import "./imagess.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aqa" style={{ backgroundImage: `url(${image1})` }}>
+      <div>
+        <Sidebar />
+        <Table />
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="side">
+      <div>All Movies</div>
+      <div>Action</div>
+      <div>Comedy</div>
+      <div>Thiller</div>
+    </div>
+  );
+}
